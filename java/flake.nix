78ea7e64479989c9e5a900d9413e7f0b9108555a
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-05-15 18:16:41 +0100

    templates/java: port to flake-parts

--- java/flake.nix
+++ java/flake.nix
@@ -3,61 +3,86 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    build-gradle-application.url = "github:raphiz/buildGradleApplication";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    make-shell.url = "github:nicknovitski/make-shell";
+    build-gradle-application = {
+      url = "github:raphiz/buildGradleApplication";
+      inputs.flake-parts.follows = "flake-parts";
+    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      build-gradle-application,
-      ...
-    }:
-    let
-      pname = "javaproj";
-      version = "0.0.1";
-      sys = "x86_64-linux";
-      # java --enable-preview --source 23 main.java
-    in
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ build-gradle-application.overlays.default ];
-        };
-        jdk = pkgs.jdk23;
-        gradle = pkgs.callPackage pkgs.gradle-packages.gradle_8 { java = jdk; };
-      in
+    { self, ... }@inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        nixpkgs,
+        withSystem,
+        ...
+      }:
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            jdt-language-server
-            java-language-server
-            gradle-completion
-            updateVerificationMetadata
-            jdk
-            gradle
-          ];
-        };
-        # Mostly copied from https://nixos.org/manual/nixpkgs/stable/#sec-language-java
-        packages = {
-          default = self.packages.${sys}.${pname};
-          ${pname} = pkgs.buildGradleApplication {
-            inherit
-              pname
-              version
-              jdk
-              gradle
-              ;
-            src = ./.;
-            meta.license = pkgs.lib.licenses.mit;
-            buildTask = "installDist";
-            installLocation = "app/build/install/*/";
+        imports = [
+          inputs.make-shell.flakeModules.default
+        ];
+
+        systems = [
+          "x86_64-linux"
+        ];
+
+        perSystem =
+          {
+            config,
+            pkgs,
+            lib,
+            system,
+            self',
+            ...
+          }:
+          let
+            pname = "javaproject";
+            version = "0-unstable";
+            jdk = pkgs.jdk23;
+            gradle = pkgs.callPackage pkgs.gradle-packages.gradle_8 { java = jdk; };
+          in
+          {
+            # Apply build-gradle-application overlay
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.build-gradle-application.overlays.default
+              ];
+            };
+            make-shells.default = {
+              packages = with pkgs; [
+                jdt-language-server
+                java-language-server
+                gradle-completion
+                updateVerificationMetadata
+                jdk
+                gradle
+              ];
+              env = {
+                # environment variables for development shell
+              };
+            };
+            packages = {
+              default = self'.packages.${pname};
+              ${pname} = pkgs.buildGradleApplication {
+                inherit
+                  pname
+                  version
+                  jdk
+                  gradle
+                  ;
+                src = ./.;
+                buildTask = "installDist";
+                installLocation = "app/build/install/*/";
+
+                meta = {
+                  license = lib.licenses.mit;
+                };
+              };
+            };
           };
-        };
       }
     );
 }
